cebc6517b2278aa404ab80328acb3f393a7ed220
quoctrungdh <quoctrungdh@gmail.com>
2016-12-11 12:24:07 +0700

    state component

---
+++ src/Timer.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class Timer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {secondsElapsed: 0};
+  }
+
+  tick() {
+    this.setState((prevState) => ({
+      secondsElapsed: prevState.secondsElapsed + 1}));
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return(
+      <div>
+        <hr />
+        <h3>A stateful component</h3>
+        Seconds elapsed: {this.state.secondsElapsed}
+        <hr />
+      </div>
+    );
+  }
+
+}
+
+export default Timer;
